ed3b330b4546a5080741c877b6f04474ccf77811
andu133 <144941878+andu133@users.noreply.github.com>
2024-06-03 12:54:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,25 +9,22 @@ let item = "";
 let btn1 = document.getElementById("btn1");
 
 btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Send tg data");
-		item = `${tg.initData}`;
-		tg.MainButton.show();
-	}
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Send tg data");
+        item = tg.initData;
+        tg.MainButton.show();
+    }
 });
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
+// Отправка данных при нажатии на кнопку
+tg.MainButton.on('click', function() {
+    tg.sendData(item);
 });
 
-
+// Обновление данных в usercard
 let usercard = document.getElementById("usercard");
-
 let p = document.createElement("p");
-
-
 p.innerText = `${tg.initData}`;
 usercard.appendChild(p);
