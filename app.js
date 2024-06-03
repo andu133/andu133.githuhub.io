2608c75a24e9f949b10598eb456cb500063f87fc
andu133 <144941878+andu133@users.noreply.github.com>
2024-06-03 12:21:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+p.innerText = `${tg.initData}
+${tg.initData}`;
 
 usercard.appendChild(p);
