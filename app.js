b2e1246e933c261d9bc16f0ca6829054ab116578
andu133 <144941878+andu133@users.noreply.github.com>
2024-06-03 12:28:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initData}
-${tg.initData}`;
+p.innerText = `${tg.initData}${tg.initData}`;
 
+usercard.appendChild(p);
+p.innerText = `TEST`;
+usercard.appendChild(p);
+
+p.innerText = `${tg.initData}`;
 usercard.appendChild(p);
