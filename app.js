2ecbe2113d356b870341b071ccfc9bd705b799f2
andu133 <144941878+andu133@users.noreply.github.com>
2024-06-03 12:42:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
 
+let item = "";
 
+let btn1 = document.getElementById("btn1");
+
+btn1.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText(`${tg.initData}`);
+		item = `${tg.initData}`;
+		tg.MainButton.show();
+	}
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
